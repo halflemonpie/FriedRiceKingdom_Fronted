69a33298cba5921d246e4cc301ca6b9d94bd5e82
Yonghai <dylanyonghaih@gmail.com>
2022-06-27 15:58:18 -0400

    re-factor for merge: working

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,8 +5,26 @@ import { Route, Routes, Link } from 'react-router-dom'
 import axios from 'axios';
 import GetData from './components/GetData';
 import NewProject from './components/NewProject'
+import Detail from './components/Detail';
 
 function App() {
+  const [data, setData] = useState([])
+  const API = "http://localhost:8080/projects"
+
+  useEffect(() => {
+    const fetchData = async () => {
+    
+      try { 
+        const {data: response} = await axios.get(API)
+        setData(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+    
+    
+    }, []);
 
   return (
     <div className="App">
@@ -21,9 +39,9 @@ function App() {
       </nav>
       <main>
       <Routes>
-        <Route path="/" element={<GetData />}/>
+        <Route path="/" element={<GetData data={data}/>}/>
         <Route path="/create" element={<NewProject />}/>
-        <Route path="/:id" element={<Detail />} />
+        <Route path="id/:id" element={<Detail />} />
       </Routes>
     </main>
     </div>
--- Frontend/src/components/GetData.jsx
+++ Frontend/src/components/GetData.jsx
@@ -3,35 +3,19 @@ import App from '../App';
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 import '../App.css'
+import { Link } from "react-router-dom";
 
-const GetData = () =>  {
-
-    const [data, setData] = useState([])
-    const API = "http://localhost:8080/projects"
-
-
-useEffect(() => {
-const fetchData = async () => {
-
-  try { 
-    const {data: response} = await axios.get(API)
-    setData(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
-fetchData();
-
-
-}, []);
+const GetData = ({data}) =>  {
 
 const mapVariables = data.map((key) => {
       return (
+        <Link to={`/id/${key._id}`}>
         <div className="cardDisplay">
                 <h5 className="cardDisplay" id="card">
                 {key.name}
                 </h5>
         </div>
+        </Link>
       )
      
       
