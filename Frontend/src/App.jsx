2e39602f47ee49d5c13411061840bda881537b7b
Jillian <jillianmatluck@gmail.com>
2022-06-28 11:40:14 -0400

    calendar draft

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import GetData from './components/GetData';
 import NewProject from './components/NewProject'
 import Detail from './components/Detail';
+import CalendarDisplay from './components/CalendarDisplay';
 
 function App() {
   const [data, setData] = useState([])
@@ -35,10 +36,14 @@ function App() {
           <input className="filter" type="text" name="filter" placeholder='Filter your projects'/>
           </div>
         </Link>
+        <Link to="/calendar">
+        <h1 className="nav">Calendar</h1> 
+        </Link>
       </nav>
       <main>
       <Routes>
         <Route path="/" element={<GetData data={data}/>}/>
+        <Route path="/calendar" element={<CalendarDisplay/>}/>
         <Route path="/create" element={<NewProject />}/>
         <Route path="id/:id" element={<Detail />} />
       </Routes>
---
+++ Frontend/src/components/CalendarDisplay.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar'
+import App from '../App';
+import 'react-calendar/dist/Calendar.css';
+
+const CalendarDisplay = () => {
+
+    const [date, setDate] = useState(new Date());
+
+    return (
+     <div >
+      <Calendar className="calendarCells" onChange={setDate} value={date} onClick={console.log("hi")} />
+    </div>
+
+    )
+
+}
+
+export default CalendarDisplay
