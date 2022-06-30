2d8742abaa5c2e1cea1b4cb20c37effadac0bb32
NelliIfraimova <ifraimovanelli@gmail.com>
2022-06-30 09:35:38 -0400

    fixing

--- Frontend/src/components/CatFilterBar.jsx
+++ Frontend/src/components/CatFilterBar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import './App.css'
 export default function CatFilterBar ({handleFilterChange}) {
     return (
         <input className="filters" onChange={handleFilterChange} type="text" name="category-filter" placeholder="filter by category"/>
