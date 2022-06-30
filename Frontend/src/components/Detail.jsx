b611a4793f1d792001ae5282df0f5724f89e6518
NelliIfraimova <ifraimovanelli@gmail.com>
2022-06-30 14:32:11 -0400

    updated details

--- Frontend/src/components/Detail.jsx
+++ Frontend/src/components/Detail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useTransition } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import './Details.css'
 
 // to-do:
 // delete todo
@@ -169,37 +170,52 @@ console.log(data)
 
 
     return (
-        <div>
+        <div className="detail-card">
+            <ul>
+
+            <li id="name">
             {(editing.name) ? <input name="name" type="text" onChange={handleChange} value={data.name} /> : <h1>{data.name}</h1>}
             {(editing.name) ? <button onClick={() => {handleSubmit("name")}}>Save</button> : <button onClick={() => {handleEdit("name")}}>Edit for name</button>}
-            
-            {(editing.image) ? <input name="image" type="text" onChange={handleChange} value={data.image} /> : <img src={data.image} alt={`image for ${data.name}`}/> }
-            {(editing.image) ? <button onClick={() => {handleSubmit("image")}}>Save</button> : <button onClick={() => {handleEdit("image")}}>Edit for Image</button>}
+            </li>
 
+            <li id="image">
+            {(editing.image) ? <input name="image" type="text" onChange={handleChange} value={data.image} /> : <img src={data.image} alt={`image for ${data.name}`}/> }
+            {(editing.image) ? <button onClick={() => {handleSubmit("image")}}>Save</button> : <button onClick={() => {handleEdit("image")}}> Edit for Image</button>}
+            </li>
+
+            <li id="date">
             {(editing.date) ? <input name="date" type="date" onChange={handleChange} value={data.date} /> : <p>Date: {data.date}</p>}
             {(editing.date) ? <button onClick={() => {handleSubmit("date")}}>Save</button> : <button onClick={() => {handleEdit("date")}}>Edit for date</button>}
-            
+            </li>
+
+            <li id="description">
             {(editing.description) ? <input name="description" type="text" onChange={handleChange} value={data.description} /> : <p>Description: {data.description}</p>}
             {(editing.description) ? <button onClick={() => {handleSubmit("description")}}>Save</button> : <button onClick={() => {handleEdit("description")}}>Edit for description</button>}
+            </li>
 
+            <li id="category">
             {(editing.category) ? <input name="category" type="text" onChange={handleChange} value={data.category} /> : <p>Category: {data.category}</p>}
             {(editing.category) ? <button onClick={() => {handleSubmit("category")}}>Save</button> : <button onClick={() => {handleEdit("category")}}>Edit for category</button>}
+            </li>
 
+            <li id="importance">
             <p>Importance Level:{data.importance}</p>
             {(editing.importance) ? <div><button onClick={() => {handleImportance("+")}}>+</button><button onClick={() => {handleImportance("-")}}>-</button><button onClick={() => {handleSubmit("importance")}}>Save</button></div> : <button onClick={() => {handleEdit("importance")}}>edit for importance</button>}
-             
-            {loading ? <p>loading</p> : <p>Complete: {data.complete.toString()}</p>}<button onClick={() => {handleBoolean("complete")}}>Click Me tO Change</button>
+            {loading ? <p>loading</p> : <p>Complete: {data.complete.toString()}</p>}<button onClick={() => {handleBoolean("complete")}}>Edit Event</button>
+            </li>
 
-            <button onClick={handleDelete}>Delete Event!</button>
-
-            {/* {loading ? <p>loading</p> : data.tasks.map((todo) => {return (<div><p>{todo.name}</p><p>{todo.complete}</p></div>)})} */}
+            <li id="deleteEvent">
+<button onClick={handleDelete}>Delete Event</button>
+            </li>
+            <li id="todoList">
+                 {/* {loading ? <p>loading</p> : data.tasks.map((todo) => {return (<div><p>{todo.name}</p><p>{todo.complete}</p></div>)})} */}
             {toDoList}
 
-            {(editing.todo) ? <div><input type="text" name="task" onChange={(e) => setTask(e.target.value)}/>  <button onClick={handleNewTodo}>add</button></div> : <button onClick={() => handleEdit("todo")}>add todo</button>}
+{(editing.todo) ? <div><input type="text" name="task" onChange={(e) => setTask(e.target.value)}/>  <button onClick={handleNewTodo}>add</button></div> : <button onClick={() => handleEdit("todo")}>add todo</button>}
 
+            </li>
             
-            
-           
+            </ul>
 
         </div>
     )
